0fb259ac168221c694a3c63ea90f868f1b2c59b1
yuki526 <yk.g187@gmail.com>
2020-12-03 19:09:17 +0900

    DB migration

--- db/migrate/20201203085150_create_recipes.rb
+++ db/migrate/20201203085150_create_recipes.rb
@@ -1,6 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
+      t.string :title,             null: false
+      t.string :URL,               null: false
+      t.integer :site_type_id,     null: false
+      t.integer :effort_level_id,  null: false
+      
+      t.references :user,          null: false,  foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_024318) do
+ActiveRecord::Schema.define(version: 2020_12_03_085150) do
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "URL", null: false
+    t.integer "site_type_id", null: false
+    t.integer "effort_level_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_12_03_024318) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
