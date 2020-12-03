43836d608eb7ebcdde80d3ab115edea1272a12b2
yuki526 <yk.g187@gmail.com>
2020-12-03 17:49:29 +0900

    調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
-  before_action :authenticate 
-
-def authenticate
-  redirect_to root_path unless user_signed_in?
-end
 
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
