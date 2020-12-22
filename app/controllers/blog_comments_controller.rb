f64ed83c08f5c51ed4cce74aa2e0734ca1baaf67
yuki526 <yk.g187@gmail.com>
2020-12-22 15:00:55 +0900

    コメントの表示

--- app/controllers/blog_comments_controller.rb
+++ app/controllers/blog_comments_controller.rb
@@ -7,7 +7,7 @@ class BlogCommentsController < ApplicationController
     @recipe = @blog.recipe
     binding.pry
     if @comment.save
-      redirect_to recipe_blog_path(recipe_id: @recipe.id, blog_id: @blog.id) and return
+      redirect_to recipe_blog_path(recipe_id: @recipe.id, id: @blog.id) and return
     else
       render "blogs/show"
     end
@@ -20,6 +20,6 @@ class BlogCommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:blog_comment).permit(:text).merge(user_id: current_user.id, blog_id: params[:recipe_id])
+    params.require(:blog_comment).permit(:text).merge(user_id: current_user.id, blog_id: params[:blog_id])
   end
 end
