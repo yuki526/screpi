b333986bc08b2d8680c2f57419c25258eb8de741
yuki526 <yk.g187@gmail.com>
2020-12-24 16:11:46 +0900

    お気に入りレシピを詳細ページの大枠

--- app/models/favorite_recipe_ingredient.rb
+++ app/models/favorite_recipe_ingredient.rb
@@ -1,4 +1,14 @@
 class FavoriteRecipeIngredient < ApplicationRecord
   belongs_to :favorite_recipe
   belongs_to :ingredient
+
+  def self.set_ingredient(recipe)
+    @ingredients_id = FavoriteRecipeIngredient.where(favorite_recipe_id: recipe.id)
+    names = Array.new
+    @ingredients_id.each do |id|
+      ingredient_name = Ingredient.find(id.ingredient_id)
+      names << ingredient_name
+    end
+    names
+  end
 end
