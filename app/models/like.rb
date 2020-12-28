c5d92fb6ecec896ad847dfd68004b04126507e02
yuki526 <yk.g187@gmail.com>
2020-12-28 20:28:03 +0900

    使わないメソッドを一旦コメントアウト

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,8 +3,8 @@ class Like < ApplicationRecord
   belongs_to :recipe
   validates_uniqueness_of :recipe_id, scope: :user_id
 
-  def already_liked?(recipe)
-    self.likes.exists?(recipe_id: recipe.id)
-  end
+  # def already_liked?(recipe)
+  #   self.likes.exists?(recipe_id: recipe.id)
+  # end
 
 end
