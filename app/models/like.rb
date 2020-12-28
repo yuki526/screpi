0dd91d0b2b506c09ef9b6f572dd1312e528e3c1b
yuki526 <yk.g187@gmail.com>
2020-12-28 20:24:25 +0900

    コントローラーの実装

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
+  validates_uniqueness_of :recipe_id, scope: :user_id
+
+  def already_liked?(recipe)
+    self.likes.exists?(recipe_id: recipe.id)
+  end
+
 end
