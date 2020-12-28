80d330725fb9a25349399fbcb20763252f704654
yuki526 <yk.g187@gmail.com>
2020-12-29 01:32:52 +0900

    素材名検索

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,10 +4,29 @@ class SearchesController < ApplicationController
     @keyword = params[:keyword]
     if @keyword != ""
       @recipes = Recipe.search(@keyword)
-      # @ingredients = Ingredient.search(@keyword)
     else
       redirect_to root_path
     end
+  end
+
+  def ingredient
+    @keyword = params[:keyword]
+    if @keyword != ""
+      ingredient = Ingredient.where('name Like ?', "#{@keyword}")
+      if ingredient.empty?
+        @recipes = nil
+      else
+        ids = RecipeIngredient.where(ingredient_id: ingredient[0].id)
+        @recipes = Array.new
+        ids.each do |id|
+          recipe = Recipe.find(id.recipe_id)
+          @recipes << recipe
+        end
+      end
+    else
+      redirect_to searches_path
+    end
+
   end
 
 
