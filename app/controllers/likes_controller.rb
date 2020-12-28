7a9e5b2c9ef03b8a759a5ee795dfb3a6e48c5552
yuki526 <yk.g187@gmail.com>
2020-12-28 18:54:11 +0900

    アクションの定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+
+  def create
+    @like = current_user.likes.create(post_id: params[:post_id])
+    redirect_to recipe_path(params[:id])
+  end
+
+  def destroy
+    @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @like.destroy
+    redirect_to recipe_path(params[:id])
+  end
+
 end
