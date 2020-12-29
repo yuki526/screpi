4de9e364372ce0a324be11d1740a0e12a576c31a
yuki526 <yk.g187@gmail.com>
2020-12-29 17:43:47 +0900

    userモデルバリデーションとrspec導入

--- app/models/user_icon.rb
+++ app/models/user_icon.rb
@@ -1,11 +1,12 @@
 class UserIcon < ActiveHash::Base
   self.data = [
-    { id: 1, name: 'far fa-smile'},      # smile
-    { id: 2, name: 'fas fa-apple-alt'},  # apple
-    { id: 3, name: 'fas fa-carrot'},     # carrot
-    { id: 4, name: 'fas fa-fish'},       # fish
-    { id: 5, name: 'fas fa-mug-hot'},    # mug
-    { id: 6, name: 'fas fa-cat'}         # cat
+    { id: 1, name: '--'},
+    { id: 2, name: 'far fa-smile'},      # smile
+    { id: 3, name: 'fas fa-apple-alt'},  # apple
+    { id: 4, name: 'fas fa-carrot'},     # carrot
+    { id: 5, name: 'fas fa-fish'},       # fish
+    { id: 6, name: 'fas fa-mug-hot'},    # mug
+    { id: 7, name: 'fas fa-cat'}         # cat
   ]
 
   include ActiveHash::Associations
