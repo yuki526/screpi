c137099b94d1d537391bf75b0cb2d94929cdb3c2
yuki526 <yk.g187@gmail.com>
2020-12-30 11:53:02 +0900

    factoryBot記述

--- spec/factories/form_recipe_ingredients.rb
+++ spec/factories/form_recipe_ingredients.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :form_recipe_ingredient do
+    title { 'test-title' }
+    url { 'https://testurl.com' }
+    site_type_id { 3 }
+    effort_level_id { 3 }
+    content { 'テスト内容テスト内容テスト内容テスト内容テスト内容テスト内容' }
+    name { 'にんじん、じゃがいも、玉ねぎ'}
     
   end
 end
