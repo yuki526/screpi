530144bbc0c93a39622929e2eab33a5556ba9b02
yuki526 <yk.g187@gmail.com>
2020-12-31 10:53:39 +0900

    ingredientのfactorybot

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -8,6 +8,10 @@ class Ingredient < ApplicationRecord
   has_many :lists
   has_many :users, through: :lists
 
+  with_options presence: true do
+    validates :name, length: { maximum: 10 }
+  end
+
 
 
   private
