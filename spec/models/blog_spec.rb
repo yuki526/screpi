6b5043ce58d9c15a8a497627e8c45fd7f88aa112
yuki526 <yk.g187@gmail.com>
2020-12-31 13:26:53 +0900

    blogモデルの単体テストコード

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = FactoryBot.create(:user)
+      @recipe = FactoryBot.create(:recipe, user_id: @user.id)
+      @blog = FactoryBot.build(:blog, user_id: @user.id, recipe_id: @recipe.id)
+    end
+
+    context "新規ブログ投稿ができる時" do
+      it "すべての項目が正しく存在すれば登録できること" do
+        expect(@blog).to be_valid
+      end
+    end
+
+    context "新規ブログ投稿ができない時" do
+      it "タイトルが空だと投稿できない" do
+        @blog.title = nil
+        @blog.valid?
+        expect(@blog.errors.full_messages).to include("タイトルを入力してください")
+      end
+
+      it "タイトルが21文字以上だと登録できない" do
+        @blog.title = "あいうえおかきくけこさしすせそたちつてとな"
+        @blog.valid?
+        expect(@blog.errors.full_messages).to include("タイトルは20文字以内で入力してください")
+      end
+
+      it "本文が空だと投稿できない" do
+        @blog.content = nil
+        @blog.valid?
+        expect(@blog.errors.full_messages).to include("本文を入力してください")
+      end
+    end
+  end
 end
